d011988fb3ee5e0b97c03aa5f100b32b711c1720
HSG424 <mrhan998@gmail.com>
2021-12-13 05:04:52 +0700

    bad code fix

--- src/components/Main/Loading.js
+++ src/components/Main/Loading.js
@@ -2,13 +2,12 @@ import classes from "./Loading.module.scss";
 import { WEATHER_STR } from "../../config.js";
 
 const Loading = (props) => {
-  const loadingType =
-    props.type === WEATHER_STR
-      ? "spinner-size-weather"
-      : "spinner-size-translate";
-
   return (
-    <div className={`spinner-border text-primary ${classes[loadingType]}`}>
+    <div
+      className={`spinner-border text-primary ${
+        classes[`spinner-size-${props.type}`]
+      }`}
+    >
       <span className="visually-hidden">Loading...</span>
     </div>
   );
